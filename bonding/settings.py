caecdf068e5527225916dd4b0e64d05ba6a269b4
hariomkumawat <er.hariomkumawat@gmail.com>
2025-12-05 22:43:51 +0530

    first 3

--- bonding/settings.py
+++ bonding/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-change-this-in
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1','bonding-backend.onrender.com').split(',')
 
 
 # Application definition
